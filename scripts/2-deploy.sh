dc65357106d664e590498c32e9b575e364499ae3
GuIInE <guuinea@gmail.com>
2022-04-18 16:23:32 +0100

    Proyect init

---
+++ scripts/2-deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eo pipefail
+ARTIFACT_BUCKET=$(cat bucket-name.txt)
+STACK_NAME=aizon-api-test-stack
+aws cloudformation package --template-file ../aws/template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
+aws cloudformation deploy --template-file out.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Environment=stage
